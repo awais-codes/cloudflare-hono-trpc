f35b7128f78e9547a0fbc45240822802e76a0550
awais-codes <awais.codes1@gmail.com>
2024-03-22 18:38:27 +0500

    fix: test case fixed

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,19 @@
-// import { Hono } from 'hono';
-// import { appRouter } from './router';
-// import { honoTRPCServer } from './trpc/hono-adapter';
+import { Hono } from 'hono';
+import { appRouter } from './router';
+import { honoTRPCServer } from './trpc/hono-adapter';
 
-// const TRPC_ENTRYPOINT_ROUTE = '/trpc';
+const TRPC_ENTRYPOINT_ROUTE = '/trpc';
 
-// const worker = new Hono();
+const worker = new Hono();
 
-// worker.use(
-// 	`${TRPC_ENTRYPOINT_ROUTE}/*`,
-// 	honoTRPCServer({
-// 		router: appRouter,
-// 		endpoint: TRPC_ENTRYPOINT_ROUTE,
-// 	})
-// );
+worker.use(
+	`${TRPC_ENTRYPOINT_ROUTE}/*`,
+	honoTRPCServer({
+		router: appRouter,
+		endpoint: TRPC_ENTRYPOINT_ROUTE,
+	})
+);
 
 // Not needed to listen to a port because that is managed by wrangler
 
-export default {
-	async fetch(request: Request, env: any, ctx: ExecutionContext): Promise<Response> {
-		console.log('request', request);
-		// return worker.fetch(request, env, ctx);
-		return new Response('Hello World!');
-	},
-};
+export default worker;
